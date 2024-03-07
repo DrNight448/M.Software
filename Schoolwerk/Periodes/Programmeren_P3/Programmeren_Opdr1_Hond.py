84c77eb1d244dfe72e392d2109ae5d5f091eff99
DrNight448 <marvinbader2007@gmail.com>
2024-03-07 14:37:41 +0100

    commit

--- Schoolwerk/Periodes/Programmeren_P3/Programmeren_Opdr1_Hond.py
+++ Schoolwerk/Periodes/Programmeren_P3/Programmeren_Opdr1_Hond.py
@@ -2,6 +2,9 @@
     Marvin Bader 
     Opdracht 1 Visual Studio Code en een klasse 
 """
+
+import random
+
 class Hond:
     def __init__(self, naam, leeftijd, geluid="Woef", ras=""):
         self.naam = naam
@@ -20,12 +23,37 @@ class Hond:
 hond1 = Hond("Princess Bubblegum", 1, "Woef woef", "Labrador")
 hond2 = Hond("Megetron Destroyer of worlds", 5, "RROAAAWWRHHH", "Golden Retriever")
 hond3 = Hond("Lilly", 3, "GRRRrrrr", "Duitse Herder")
+hond4 = Hond("Lordvoldemort", 5, "Woof", "Duitse Herder")
+hond5 = Hond("Lilly", 3, "GRRrrrrbhh", "Duitse Herder")
+hond6 = Hond("Billy", 3, "GRRRawwrrrr", "Duitse Herder")
+hond7 = Hond("Silly", 3, "GRRRuurrrrrr", "Duitse Herder")
+hond8 = Hond("Lolly", 3, "GRRRieerrrrr", "Duitse Herder")
+hond9 = Hond("Jilly", 3, "GRRReeeeerrrr", "Duitse Herder")
+hond10 = Hond("Jolly", 3, "GRRRerrrr", "Duitse Herder")
 
-hond1.blaf()
-hond2.blaf()
-hond3.blaf()
 
-hondenlijst = [hond1, hond2, hond3]
-
-for hond in hondenlijst:
-    hond.blaf()
+blafLoop = 0
+while blafLoop != 10:
+    blafLoop += 1
+    x = random.randint(1, 10)
+    match x:
+        case 1:
+            hond1.blaf()
+        case 2:
+            hond2.blaf()
+        case 3:
+            hond3.blaf()
+        case 4:
+            hond4.blaf()
+        case 5:
+            hond5.blaf()
+        case 6:
+            hond6.blaf()
+        case 7:
+            hond7.blaf()
+        case 8:
+            hond8.blaf()
+        case 9:
+            hond9.blaf()
+        case 10:
+            hond10.blaf()
