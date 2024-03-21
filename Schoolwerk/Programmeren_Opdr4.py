ca0ef1160d2ff2a923b98d2d43c0290aafc57146
DrNight448 <marvinbader2007@gmail.com>
2024-03-21 13:02:11 +0100

    second commit

--- Schoolwerk/Programmeren_Opdr4.py
+++ Schoolwerk/Programmeren_Opdr4.py
@@ -3,4 +3,17 @@
     Software development
     Opdracht 4: Een gui met Tkinter
 """
+import tkinter as tk
+window = tk.Tk()
+label = tk.Label(text="Eerste GUI met Tkinter!")
+label.pack()
 
+
+
+
+entry = tk.Entry(fg="white", bg="blue", width=50)
+entry.pack()
+
+entry.insert(0, "Wa is juh naam a driller")
+
+window.mainloop()
