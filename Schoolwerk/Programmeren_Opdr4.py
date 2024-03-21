3645354d06004de8b713ab008633e28d1b3fd535
DrNight448 <marvinbader2007@gmail.com>
2024-03-21 13:39:43 +0100

    Last commit

--- Schoolwerk/Programmeren_Opdr4.py
+++ Schoolwerk/Programmeren_Opdr4.py
@@ -5,15 +5,19 @@
 """
 import tkinter as tk
 window = tk.Tk()
+
+    #Hier staat de introductie van de applicatie
 label = tk.Label(text="Eerste GUI met Tkinter!")
 label.pack()
 
-
-
-
-entry = tk.Entry(fg="white", bg="blue", width=50)
+    #hier kan iemand ze naam invoeren
+entry = tk.Entry(fg="white", bg="blue", width=50,)
+entry.insert(0, "Wa is juh naam a driller")
 entry.pack()
 
-entry.insert(0, "Wa is juh naam a driller")
-
+    #Hier kan iemand in typen
+label = tk.Label(text="Hier onder kan je typen", fg="red", bg="gray",height=1, width=50,)
+label.pack()
+text_box = tk.Text(fg="orange", bg="black",height=4, width=50,)
+text_box.pack()
 window.mainloop()
